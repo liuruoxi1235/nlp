b388cd6cd57bb4bc74d2b0427fbab3373f1972f4
stonehj05 <stonehj@outlook.com>
2024-08-29 15:38:38 -0400

    full version for hw1 coding

---
+++ hw1/randsent_haojun.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python3
+"""
+601.465/665 — Natural Language Processing
+Assignment 1: Designing Context-Free Grammars
+
+Assignment written by Jason Eisner
+Modified by Kevin Duh
+Re-modified by Alexandra DeLucia
+
+Code template written by Alexandra DeLucia,
+based on the submitted assignment with Keith Harrigian
+and Carlos Aguirre Fall 2019
+"""
+import os
+import sys
+import random
+import argparse
+
+# Want to know what command-line arguments a program allows?
+# Commonly you can ask by passing it the --help option, like this:
+#     python randsent.py --help
+# This is possible for any program that processes its command-line
+# arguments using the argparse module, as we do below.
+#
+# NOTE: When you use the Python argparse module, parse_args() is the
+# traditional name for the function that you create to analyze the
+# command line.  Parsing the command line is different from parsing a
+# natural-language sentence.  It's easier.  But in both cases,
+# "parsing" a string means identifying the elements of the string and
+# the roles they play.
+
+def parse_args():
+    """
+    Parse command-line arguments.
+
+    Returns:
+        args (an argparse.Namespace): Stores command-line attributes
+    """
+    # Initialize parser
+    parser = argparse.ArgumentParser(description="Generate random sentences from a PCFG")
+    # Grammar file (required argument)
+    parser.add_argument(
+        "-g",
+        "--grammar",
+        type=str, required=True,
+        help="Path to grammar file",
+    )
+    # Start symbol of the grammar
+    parser.add_argument(
+        "-s",
+        "--start_symbol",
+        type=str,
+        help="Start symbol of the grammar (default is ROOT)",
+        default="ROOT",
+    )
+    # Number of sentences
+    parser.add_argument(
+        "-n",
+        "--num_sentences",
+        type=int,
+        help="Number of sentences to generate (default is 1)",
+        default=1,
+    )
+    # Max number of nonterminals to expand when generating a sentence
+    parser.add_argument(
+        "-M",
+        "--max_expansions",
+        type=int,
+        help="Max number of nonterminals to expand when generating a sentence",
+        default=450,
+    )
+    # Print the derivation tree for each generated sentence
+    parser.add_argument(
+        "-t",
+        "--tree",
+        action="store_true",
+        help="Print the derivation tree for each generated sentence",
+        default=False,
+    )
+    return parser.parse_args()
+
+
+class Grammar:
+    def __init__(self, grammar_file):
+        """
+        Context-Free Grammar (CFG) Sentence Generator
+
+        Args:
+            grammar_file (str): Path to a .gr grammar file
+        
+        Returns:
+            self
+        """
+        # Parse the input grammar file
+        self.rules = None
+        self._load_rules_from_file(grammar_file)
+        print(self.rules)
+
+    def _load_rules_from_file(self, grammar_file):
+        """
+        Read grammar file and store its rules in self.rules
+
+        Args:
+            grammar_file (str): Path to the raw grammar file 
+        """
+        self.rules = {}
+        with open(grammar_file, "r") as file:
+            for line in file:
+                if not line.strip():
+                    continue
+                if line[0] == "#":
+                    continue
+                substrings = line.strip().split()
+                if substrings[1] not in self.rules.keys():
+                    self.rules[substrings[1]] = {"weights": [float(substrings[0])], "rules": []}
+                else:
+                    self.rules[substrings[1]]["weights"].append(float(substrings[0]))
+                temp_list = []
+                for substring in substrings[2:]:
+                    if "#" in substring:
+                        break
+                    temp_list.append(substring)
+                self.rules[substrings[1]]["rules"].append(temp_list)
+        for rule in self.rules.keys():
+            total = 0
+            for weight in self.rules[rule]["weights"]:
+                total += weight
+            self.rules[rule]["total_weight"] = total
+
+
+
+    def sample(self, derivation_tree, max_expansions, start_symbol):
+        """
+        Sample a random sentence from this grammar
+
+        Args:
+            derivation_tree (bool): if true, the returned string will represent 
+                the tree (using bracket notation) that records how the sentence 
+                was derived
+                               
+            max_expansions (int): max number of nonterminal expansions we allow
+
+            start_symbol (str): start symbol to generate from
+
+        Returns:
+            str: the random sentence or its derivation tree
+        """
+        ans = ""
+        total_expansion = 0
+        def expand(current_expansion, max_expansions, start_symbol):
+            nonlocal ans, total_expansion
+            if current_expansion > max_expansions:
+                ans += "..."
+                ans += " "
+                return
+            if start_symbol not in self.rules.keys():
+                ans += start_symbol
+                ans += " "
+                return
+            if derivation_tree:
+                ans += "("
+                ans += start_symbol
+                ans += " "
+            total_expansion += 1
+            expand_rule = random.choices(self.rules[start_symbol]["rules"], weights=self.rules[start_symbol]["weights"])
+            for element in expand_rule[0]:
+                expand(total_expansion, max_expansions, element)
+            if derivation_tree:
+                ans += ")"
+                
+            
+        expand(0, max_expansions, start_symbol)
+        return ans
+
+
+####################
+### Main Program
+####################
+def main():
+    # Parse command-line options
+    args = parse_args()
+
+    # Initialize Grammar object
+    grammar = Grammar(args.grammar)
+
+    # Generate sentences
+    for i in range(args.num_sentences):
+        # Use Grammar object to generate sentence
+        sentence = grammar.sample(
+            derivation_tree=args.tree,
+            max_expansions=args.max_expansions,
+            start_symbol=args.start_symbol
+        )
+
+        # Print the sentence with the specified format.
+        # If it's a tree, we'll pipe the output through the prettyprint script.
+        if args.tree:
+            print(sentence)
+            prettyprint_path = os.path.join(os.getcwd(), 'prettyprint')
+            t = os.system(f"echo '{sentence}' | perl {prettyprint_path}")
+        else:
+            print(sentence)
+
+
+if __name__ == "__main__":
+    main()
